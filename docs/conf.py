6b6b5b22cf5e84f8bafc625b183957a2a3c19ae9
Nauja <jeremymorosi@hotmail.fr>
2019-04-14 20:18:01 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../pysachi'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
